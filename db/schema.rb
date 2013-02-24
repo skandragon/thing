ea40d4addd5180e4650c0448abcf11cb17186960
Michael Graff <explorer@flame.org>
2013-02-23 22:12:49 -0600

    regen

--- db/schema.rb
+++ db/schema.rb
@@ -88,8 +88,8 @@ ActiveRecord::Schema.define(:version => 20130208193322) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -98,11 +98,11 @@ ActiveRecord::Schema.define(:version => 20130208193322) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.string   "name"
     t.string   "access_token"
-    t.boolean  "admin",          :default => false
+    t.boolean  "admin",                  :default => false
     t.string   "coordinator_tract"
   end
 
