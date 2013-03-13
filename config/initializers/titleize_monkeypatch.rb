d679791dade46bdbdda1d652d8bcb0fd3ae0e160
Michael Graff <explorer@flame.org>
2013-03-13 18:36:01 -0400

    map thl to THL in titleize monkey patch

--- config/initializers/titleize_monkeypatch.rb
+++ config/initializers/titleize_monkeypatch.rb
@@ -2,8 +2,9 @@
 
 module ActiveSupport::Inflector
   def titleize_with_sca(word)
+    return 'THL' if word == 'thl'
     titleize_without_sca(word).gsub(/æ/, "Æ")
   end
-    
+
   alias_method_chain :titleize, :sca
 end
