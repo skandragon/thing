23e1e849997b5a469f630d862ac3b5944771083c
Michael Graff <explorer@flame.org>
2013-03-14 10:19:00 -0400

    add a short time display format which omits the year

--- config/initializers/default_date_format.rb
+++ config/initializers/default_date_format.rb
@@ -1,3 +1,7 @@
 Time::DATE_FORMATS[:pennsic] = "%Y-%m-%d %H:%M"
 DateTime::DATE_FORMATS[:pennsic] = "%Y-%m-%d %H:%M"
 Date::DATE_FORMATS[:pennsic] = "%Y-%m-%d"
+
+Time::DATE_FORMATS[:pennsic_short] = "%m-%d %H:%M"
+DateTime::DATE_FORMATS[:pennsic_short] = "%m-%d %H:%M"
+Date::DATE_FORMATS[:pennsic_short] = "%m-%d"
