41e80725eddf45c030bc9540b506ac13d93bce41
Michael Graff <explorer@flame.org>
2013-04-17 15:44:48 -0500

    add script to move classes from merchant-version of Touch the Earth to track one

---
+++ bin/update_touch_the_earth.rb
@@ -0,0 +1,23 @@
+ids = []
+
+instructables = Instructable.where(location_type: ['merchant-booth', 'private-camp']).where("camp_name ilike 'touch the earth'")
+instructables.each do |instructable|
+  instructable.location_type = 'track'
+  instructable.save!
+  
+  instructable.reload
+
+  instructable.instances.each do |instance|
+    instance.location = "Touch The Earth"
+    instance.save!
+  end
+
+  puts instructable.id
+  puts instructable.name
+  puts instructable.track
+  puts instructable.instances.pluck(:location).join(", ")
+  puts
+  ids << instructable.id
+end
+
+puts ids.join(", ")
