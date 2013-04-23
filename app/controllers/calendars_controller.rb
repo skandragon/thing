4f820b73019e35601af11b049b2e8ca9ad8502b1
Michael Graff <explorer@flame.org>
2013-04-23 09:02:45 -0700

    adjust spacing

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -306,51 +306,51 @@ class CalendarsController < ApplicationController
   end
 
   def render_xlsx(filename)
-      p = Axlsx::Package.new
-      p.use_shared_strings = true
+    p = Axlsx::Package.new
+    p.use_shared_strings = true
 
-      wb = p.workbook
+    wb = p.workbook
 
-      wb.styles do |s|
-        header_style = s.add_style bg_color: "00", fg_color: "FF"
-        date_format = wb.styles.add_style format_code: 'MM-DD hh:mm'
+    wb.styles do |s|
+      header_style = s.add_style bg_color: "00", fg_color: "FF"
+      date_format = wb.styles.add_style format_code: 'MM-DD hh:mm'
 
-        column_names = %W(
-          name track culture formatted_topic
-          adult_only repeat_count updated_at
-          description_book description_web
-          duration fee_itemization handout_fee handout_limit material_fee
-          material_limit
-        )
-        header = ['id', 'location', 'start_time', 'end_time', 'instructor' ] + column_names
+      column_names = %W(
+        name track culture formatted_topic
+        adult_only repeat_count updated_at
+        description_book description_web
+        duration fee_itemization handout_fee handout_limit material_fee
+        material_limit
+      )
+      header = ['id', 'location', 'start_time', 'end_time', 'instructor' ] + column_names
 
-        wb.add_worksheet(name: "Pennsic #{PENNSIC_YEAR}") do |sheet|
-          sheet.add_row header
+      wb.add_worksheet(name: "Pennsic #{PENNSIC_YEAR}") do |sheet|
+        sheet.add_row header
 
-          for instance in @instances
-            instructable = instance.instructable
-            user = instructable.user
-            start_time = Time.at(instance.start_time.to_f + instance.start_time.utc_offset.to_f)
-            end_time = Time.at(instance.end_time.to_f + instance.end_time.utc_offset.to_f)
-            data = [instructable.id, instance.formatted_location, start_time, end_time, instructable.titled_sca_name ]
-            column_names.each do |column_name|
-              data += [ instructable.send(column_name) ]
-            end
-
-            sheet.add_row data, style: [
-              nil, nil, date_format, date_format, nil,
-              nil, nil, nil, nil, nil, nil, date_format,
-            ]
-            sheet.column_widths 4, 10, 10, 10, nil, nil, nil, nil, nil, nil, 4, 6
+        for instance in @instances
+          instructable = instance.instructable
+          user = instructable.user
+          start_time = Time.at(instance.start_time.to_f + instance.start_time.utc_offset.to_f)
+          end_time = Time.at(instance.end_time.to_f + instance.end_time.utc_offset.to_f)
+          data = [instructable.id, instance.formatted_location, start_time, end_time, instructable.titled_sca_name ]
+          column_names.each do |column_name|
+            data += [ instructable.send(column_name) ]
           end
 
-          sheet.row_style 0, header_style
+          sheet.add_row data, style: [
+            nil, nil, date_format, date_format, nil,
+            nil, nil, nil, nil, nil, nil, date_format,
+          ]
+          sheet.column_widths 4, 10, 10, 10, nil, nil, nil, nil, nil, nil, 4, 6
         end
-      end
 
-      data = p.to_stream.read
-      send_data(data, type: Mime::XLSX, filename: filename)
+        sheet.row_style 0, header_style
+      end
     end
+
+    data = p.to_stream.read
+    send_data(data, type: Mime::XLSX, filename: filename)
+  end
 
   def cache_in_file(cache_filename, data)
     if cache_filename
