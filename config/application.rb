d4941da63717ffa8fdce1711bda9323cc52fd2d7
Michael Graff <explorer@flame.org>
2013-04-25 09:02:13 -0700

    tailor generator a bit more

--- config/application.rb
+++ config/application.rb
@@ -20,10 +20,17 @@ module Thing
 
   # Customize generators
   config.generators do |g|
-    g.test_framework :rspec
+    g.test_framework :rspec,
+      fixtures: true,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      conroller_specs: true,
+      request_specs: true
+    g.fixture_replacement :factory_girl, :dir => 'spec/factories',
+
     g.template_engine :haml
     g.stylesheets false
-    g.fixture_replacement :factory_girl, :dir => 'spec/factories'
   end
 
   # Additional filter elements
