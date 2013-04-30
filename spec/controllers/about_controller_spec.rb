67c459178168bf4d66450d9d1aaa60c3ce1dd17d
Michael Graff <explorer@flame.org>
2013-04-30 18:02:10 -0500

    just look for stuff within pages, not hero-unit, for now

--- spec/controllers/about_controller_spec.rb
+++ spec/controllers/about_controller_spec.rb
@@ -5,12 +5,12 @@ describe AboutController do
     describe "when not logged in" do
       it "shows a sign up link" do
         visit '/'
-        find('.hero-unit').should have_link 'Sign up'
+        page.should have_link 'Sign up'
       end
 
       it "shows a sign in link" do
         visit '/'
-        find('.hero-unit').should have_link 'Sign in'
+        page.should have_link 'Sign in'
       end
     end
 
@@ -18,13 +18,13 @@ describe AboutController do
       it "should show a become instructor link when not an instructor" do
         log_in
         visit '/'
-        find('.hero-unit').should have_link 'Become an instructor'
+        page.should have_link 'Become an instructor'
       end
 
       it "should show an update profile link for instructors" do
         log_in instructor: true
         visit '/'
-        find('.hero-unit').should have_link 'Update instructor profile'
+        page.should have_link 'Update instructor profile'
       end
     end
   end
