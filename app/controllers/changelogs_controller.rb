97d96c30dd66e313add09314479d17bbfe9439d2
Michael Graff <explorer@flame.org>
2013-07-22 13:24:19 -0500

    link in changlog.  Add a previous/next link.

--- app/controllers/changelogs_controller.rb
+++ app/controllers/changelogs_controller.rb
@@ -1,6 +1,14 @@
 class ChangelogsController < ApplicationController
   def show
-    @date = Time.zone.parse(params[:id])
-    @changelog = Changelog.changes_since
+    if params[:id].present?
+      date = params[:id]
+      if date.blank? or date == 'today'
+        date = Time.zone.now.strftime('%Y-%m-%d')
+      end
+      @date = Time.zone.parse(date)
+    else
+      @date = Time.zone.now unless date
+    end
+    @changelog = Changelog.changes_since(@date)
   end
 end
