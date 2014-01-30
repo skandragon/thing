d4d2783d2440f6d29cd0915f35a48034b0360de8
Michael Graff <explorer@flame.org>
2014-01-29 19:41:59 -0600

    test before commit, next time

--- spec/support/user_sessions.rb
+++ spec/support/user_sessions.rb
@@ -1,9 +1,9 @@
 module AuthMacros
   def log_in(attributes = {})
     if attributes[:instructor]
-      @_current_user = create(:instructor, attributes)
+      @_current_user = build(:instructor, attributes)
     else
-      @_current_user = create(:user, attributes)
+      @_current_user = build(:user, attributes)
     end
 
     if @_current_user.needs_profile?
@@ -11,6 +11,8 @@ module AuthMacros
         @_current_user.profile_updated_at = Time.now
       end
     end
+
+    @_current_user.save!
 
     visit new_user_session_path
     page.should have_content 'Remember me'
