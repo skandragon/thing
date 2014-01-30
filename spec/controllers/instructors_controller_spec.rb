83eab6b6c0b2a203d5e65476e53432ea4684478e
Michael Graff <explorer@flame.org>
2014-01-30 14:21:10 -0600

    flesh out instructor list tests

--- spec/controllers/instructors_controller_spec.rb
+++ spec/controllers/instructors_controller_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe InstructorsController do
+  before :each do
+    @instructor1 = create(:instructor, kingdom: 'ansteorra')
+    create(:instructable, user_id: @instructor1.id)
 
+    @instructor2 = create(:instructor, kingdom: 'calontir')
+    create(:instructable, user_id: @instructor2.id)
+
+    @classless_instructor1 = create(:instructor, kingdom: 'trimaris')
+
+    visit instructors_path
+  end
+
+  it 'should show names' do
+    page.should have_content @instructor1.titled_sca_name
+    page.should have_content @instructor2.titled_sca_name
+    page.should_not have_content @classless_instructor1.titled_sca_name
+  end
+
+  it 'should show names by kingdom' do
+    click_on 'By kingdom'
+    page.should have_content @instructor1.kingdom.titleize
+    page.should have_content @instructor1.titled_sca_name
+    page.should have_content @instructor2.kingdom.titleize
+    page.should have_content @instructor2.titled_sca_name
+    page.should_not have_content @classless_instructor1.kingdom.titleize
+    page.should_not have_content @classless_instructor1.titled_sca_name
+  end
 end
