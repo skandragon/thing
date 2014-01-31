5190dd7f7d81239a2e7fb27b5977ba38e69206c8
Michael Graff <explorer@flame.org>
2014-01-31 11:53:22 -0600

    start on a more advanced, easier to use differ

--- app/models/changelog.rb
+++ app/models/changelog.rb
@@ -282,6 +282,57 @@ class Changelog < ActiveRecord::Base
     ret
   end
 
+  def generate_differences(a = original, b = committed, bi_directional = true)
+    return [a.class.name, nil] if !a.nil? && b.nil?
+    return [nil, b.class.name] if !b.nil? && a.nil?
+
+    differences = {}
+    a.each do |k, v|
+      if !v.nil? && b[k].nil?
+        differences[k] = [v, nil]
+        next
+      elsif !b[k].nil? && v.nil?
+        differences[k] = [nil, b[k]]
+        next
+      end
+
+      if v.is_a?(Hash)
+        unless b[k].is_a?(Hash)
+          differences[k] = "Different types"
+          next
+        end
+        diff = different?(a[k], b[k])
+        differences[k] = diff if !diff.nil? && diff.count > 0
+
+      elsif v.is_a?(Array)
+        unless b[k].is_a?(Array)
+          differences[k] = "Different types"
+          next
+        end
+
+        c = 0
+        diff = v.map do |n|
+          if n.is_a?(Hash)
+            diffs = different?(n, b[k][c])
+            c += 1
+            diffs unless diffs.nil?
+          else
+            c += 1
+            {n: b[c] } unless b[c] == n
+          end
+        end.compact
+
+        differences[k] = diff if diff.count > 0
+
+      else
+        differences[k] = [v, b[k]] unless v == b[k]
+
+      end
+    end
+
+    return differences if !differences.nil? && differences.count > 0
+  end
+
   private
 
   def abort_if_useless
