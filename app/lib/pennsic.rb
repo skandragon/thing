75935b5bd16e2716cee23d78766c99d131286d7c
Michael Graff <explorer@flame.org>
2014-02-02 17:43:33 -0600

    start on the master object, akin to 'rails', which will hold config info

---
+++ app/lib/pennsic.rb
@@ -0,0 +1,9 @@
+class Pennsic
+  def self.year
+    43
+  end
+
+  def self.calendar_year
+    Time.now.year
+  end
+end
---
+++ spec/lib/pennsic_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Pennsic do
+  it "returns the current year" do
+    Pennsic.year.should >= 43
+  end
+
+  it 'returns the current calendar_year' do
+    Pennsic.calendar_year.should == Time.now.year
+  end
+end
