f5ea43fc6d5e53041b98f85b05e53e7e89611392
Michael Graff <explorer@flame.org>
2014-07-10 14:56:16 -0500

    use the 2014 data

--- app/lib/csv_compare.rb
+++ app/lib/csv_compare.rb
@@ -41,7 +41,7 @@ class CsvCompare
     @created_instances_for_date = []
     @changed_instances_for_date = []
 
-    book = parse_file('pennsic-42-book.csv')
+    book = parse_file('pennsic-book.csv')
     now = parse_file(filename)
 
     compare_all_entries(book, now)
