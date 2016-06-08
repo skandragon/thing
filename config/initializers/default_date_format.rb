a68b9a224daf6ad3a807fd52eafa4d7c77e337cf
Michael Graff <explorer@flame.org>
2016-06-08 08:52:38 -0500

    change to add a space in times

--- config/initializers/default_date_format.rb
+++ config/initializers/default_date_format.rb
@@ -1,10 +1,10 @@
-Time::DATE_FORMATS[:pennsic] = '%Y-%m-%d %l:%M%p'
-DateTime::DATE_FORMATS[:pennsic] = '%Y-%m-%d %l:%M%p'
+Time::DATE_FORMATS[:pennsic] = '%Y-%m-%d %l:%M %p'
+DateTime::DATE_FORMATS[:pennsic] = '%Y-%m-%d %l:%M %p'
 Date::DATE_FORMATS[:pennsic] = '%Y-%m-%d'
 
-Time::DATE_FORMATS[:pennsic_short] = '%m-%d %l:%M%p'
-DateTime::DATE_FORMATS[:pennsic_short] = '%m-%d %l:%M%p'
+Time::DATE_FORMATS[:pennsic_short] = '%m-%d %l:%M %p'
+DateTime::DATE_FORMATS[:pennsic_short] = '%m-%d %l:%M %p'
 Date::DATE_FORMATS[:pennsic_short] = '%m-%d'
 
-Time::DATE_FORMATS[:pennsic_time_only] = '%l:%M%p'
-DateTime::DATE_FORMATS[:pennsic_time_only] = '%l:%M%p'
+Time::DATE_FORMATS[:pennsic_time_only] = '%l:%M %p'
+DateTime::DATE_FORMATS[:pennsic_time_only] = '%l:%M %p'
