b65cd748bdebd591ffaa8ddeda673d45f96b582b
Michael Graff <michael.graff@nominum.com>
2018-01-08 00:11:57 -0800

    bump to ruby 2.4.3

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :branch, 'master'
 set :deploy_via, :remote_cache
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '2.4.2'
+set :rbenv_ruby, '2.4.3'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
