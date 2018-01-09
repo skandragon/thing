efee3bc8aef0f7677a0f97d3fc9987d4a25eabd4
Michael Graff <michael.graff@nominum.com>
2018-01-08 19:09:20 -0800

    bump policy version by one

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -1,5 +1,5 @@
 class Policy < ApplicationRecord
-  CURRENT_VERSION = '20170201-000000'
+  CURRENT_VERSION = '20170201-000001'
 
   def self.has_current_policy?(user)
     user && Policy.where(version: CURRENT_VERSION, user_id: user.id).count > 0
