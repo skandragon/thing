c9a404b08ae6a9d6d05547b03c097a8e189aa147
Michael Graff <explorer@flame.org>
2018-01-30 12:13:17 -0800

    move config from an initializer to the environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,13 +53,28 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
+  config.active_job.queue_adapter     = :sidekiq
   # config.active_job.queue_name_prefix = "thing_#{Rails.env}"
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "sca.flame.org" }
+  config.action_mailer.smtp_settings = {
+        address: "postmark.flame.org",
+        port: 587,
+        domain: 'sca.flame.org',
+        user_name: Rails.application.secrets.smtp_username,
+        password: Rails.application.secrets.smtp_password,
+        authentication: :plain,
+        enable_starttls_auto: true,
+        openssl_verify_mode: 'none'
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
--- config/initializers/action_mailer_setup.rb
+++ config/initializers/action_mailer_setup.rb
@@ -3,14 +3,3 @@ if ["production", "development"].include?(Rails.env)
 else
   ActionMailer::Base.delivery_method = :test
 end
-
-ActionMailer::Base.smtp_settings = {
-    :address              => "postmark.flame.org",
-    :port                 => 587,
-    :domain               => "flame.org",
-    :user_name            => MultaArcana::secret_for(:smtp_username),
-    :password             => MultaArcana::secret_for(:smtp_password),
-    :authentication       => "plain",
-    :enable_starttls_auto =>  true,
-    :openssl_verify_mode  => 'none',
-}
