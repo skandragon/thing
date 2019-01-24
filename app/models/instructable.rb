c61787f6a6cb693dd4bed427c8ac96c57638115a
Michael Graff <explorer@flame.org>
2019-01-24 00:23:39 -0800

    add Courtesan College

--- app/models/instructable.rb
+++ app/models/instructable.rb
@@ -166,6 +166,7 @@ class Instructable < ApplicationRecord
     ],
     'Party' => ['Battlefield', 'Great Hall', 'Rune Stone Park'],
     'Court' => ['Great Hall'],
+    'Courtesan College' => [],
     'Event' => ['Great Hall', 'Rune Stone Park'],
     "Artisan's Row" => [
       "Artisan's Row A",
