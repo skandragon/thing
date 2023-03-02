601d1b74fc189b83bc4b54429b85c9c9c2d1a89e
JJ Ryan <jjryan111@yahoo.com>
2023-03-02 07:23:26 -0600

    Trying more mail server settings again4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,8 @@ Rails.application.configure do
   config.active_job.queue_adapter     = :sidekiq
   # config.active_job.queue_name_prefix = "thing_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+#  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "thing.pennsicuniversity.org" }
