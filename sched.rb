019e8f425edb95578ab7919befae000448c63dd2
jj <jjryan111@yahoo.com>
2023-07-04 06:56:49 -0400

    DOes this affect book pull?

--- sched.rb
+++ sched.rb
@@ -37,7 +37,7 @@ entries = {}
 @morning_hours =   [  9, 10, 11, 12, 13 ]
 @afternoon_hours = [ 14, 15, 16, 17, 18 ]
 
-ANS_ROOMS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 19].map { |x| "A&S #{x}"}
+ANS_ROOMS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19].map { |x| "A&S #{x}"}
 
 if @schedule == "Pennsic University"
   @locs1 = ANS_ROOMS
